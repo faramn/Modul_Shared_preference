dea0a3e4c42476dccac597260b3e83c0ca23ce35
fara maulidia <faramaulidia79@gmail.com>
2019-04-12 12:24:54 +0700

    pengumpulan tugas shared preference

---
+++ NotesApp/app/src/main/java/com/ramadini/notesapp/Settings.java
@@ -0,0 +1,40 @@
+package com.ramadini.notesapp;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+
+public class Settings {
+
+    SharedPreferences preferences;
+
+    public Settings(Context context) {
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public String getUser() {
+        return preferences.getString(Constant.SESSION, null);
+    }
+
+    public void setUser(String user) {
+        preferences.edit()
+                .putString(Constant.SESSION, user)
+                .apply();
+    }
+
+    public void removeUser() {
+        preferences.edit()
+                .remove(Constant.SESSION)
+                .apply();
+    }
+
+    public int getLayoutMode() {
+        return preferences.getInt(Constant.LAYOUT_MODE, Constant.LAYOUT_MODE_LIST);
+    }
+
+    public void setLayoutMode(int layout) {
+        preferences.edit()
+                .putInt(Constant.LAYOUT_MODE, layout)
+                .apply();
+    }
+}
